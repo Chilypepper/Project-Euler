38720637cf754b158a45d5aea11d961ac9df71d3
chilypepper <peter.a.edin@gmail.com>
2015-03-31 10:58:19 -0400

    Restructed Project

---
+++ Problem_1/problem1.cpp
@@ -0,0 +1,15 @@
+//Project Euler Problem 1
+//Peter Edin
+#include <iostream>
+int main()
+{
+  int multiple1=3;
+  int multiple2=5;
+  int count=0;
+  for(int i=1;i<1000;i++){
+    if(i%multiple1==0 || i%multiple2==0){
+      count+=i;
+    }
+  }
+  std::cout<<count<<"\n";
+}
